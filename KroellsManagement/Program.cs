9780acfbde0571e9eb33631058469471ed3fd51b
Jorjo6712 <93548609+Jorjo6712@users.noreply.github.com>
2023-10-04 19:15:58 +0200

    update
    lol

--- KroellsManagement/Program.cs
+++ KroellsManagement/Program.cs
@@ -1,13 +1,14 @@
 using KroellsManagement.Data;
 using Microsoft.AspNetCore.Components;
 using Microsoft.AspNetCore.Components.Web;
+using Microsoft.Data.SqlClient;
+using Microsoft.Extensions.Configuration;
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddRazorPages();
 builder.Services.AddServerSideBlazor();
-builder.Services.AddSingleton<WeatherForecastService>();
 
 var app = builder.Build();
 
@@ -17,6 +18,14 @@ if (!app.Environment.IsDevelopment())
     app.UseExceptionHandler("/Error");
 }
 
+var config = new ConfigurationBuilder()
+        .AddJsonFile("appsettings.json")
+        .Build();
+
+// var conn = config.GetConnectionString("DefaultConnection");
+//qlConnection sqlcon = new SqlConnection (conn);
+
+//sqlcon.Open(); 
 
 app.UseStaticFiles();
 
